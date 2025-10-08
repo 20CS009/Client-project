44f8fcd52448d055f9f6f718ae4df0dd87fc0f8a
Dintakurthy Avinashswamy <86527011+20CS009@users.noreply.github.com>
2025-10-08 09:56:39 +0530

    Delete cpms/Client-project/cpms/cpms directory

--- cpms/Client-project/cpms/cpms/src/main/java/com/example/cpms/exception/JwtTokenException.java
+++
@@ -1,7 +0,0 @@
-package com.example.cpms.exception;
-
-public class JwtTokenException extends RuntimeException {
-    public JwtTokenException(String message) {
-        super(message);
-    }
-}
--- cpms/Client-project/cpms/cpms/src/main/java/com/example/cpms/exception/ResourceException.java
+++
@@ -1,7 +0,0 @@
-package com.example.cpms.exception;
-
-public class ResourceException extends RuntimeException {
-    public ResourceException(String message) {
-        super(message);
-    }
-}
